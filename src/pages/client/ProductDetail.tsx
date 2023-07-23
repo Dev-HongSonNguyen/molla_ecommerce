e14f1362e2abc9e1bf5cead73cf9edea0ea25baf
nguyenhongson <nguyenhongson20433@gmail.com>
2023-07-23 17:49:28 +0700

    fe/nav-account

--- src/pages/client/ProductDetail.tsx
+++ src/pages/client/ProductDetail.tsx
@@ -76,32 +76,7 @@ const ProductDetail = () => {
           <div className="detail-book-elem-bottom">
             <div className="detail-book-elem-bottom-box">
               <h3>Product Information</h3>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio
-                ullam sit architecto incidunt unde esse fuga nesciunt a, ab,
-                reiciendis accusamus, dicta aliquam doloremque minima harum
-                neque eum! Vitae numquam exercitationem possimus eligendi ullam
-                saepe obcaecati! Deserunt dolor eveniet ex tempore fuga sit
-                cumque, eaque itaque maxime quis exercitationem odit nihil?
-                Dicta qui corrupti aperiam eos, officia nihil expedita sequi. Ad
-                at officiis velit enim cum nemo commodi neque cupiditate,
-                deserunt quis a officia possimus repudiandae id atque, qui
-                voluptatum ab earum, doloribus distinctio facilis modi optio!
-                Fugit, iusto deleniti nisi quasi sequi labore quod doloribus
-                corrupti doloremque ab sapiente minima tempora iure ipsum
-                temporibus numquam ut quibusdam sit. Possimus fugit commodi
-                omnis veniam perferendis minus amet veritatis ab assumenda
-                minima iusto voluptatum quae eligendi accusamus, atque sed
-                asperiores adipisci quas reprehenderit! Sunt natus voluptatibus
-                ipsum iure. Mollitia, ipsam ipsum pariatur nemo obcaecati, eos
-                dolorem molestiae esse magni rerum amet officia eaque ea,
-                corrupti sed voluptates iste praesentium asperiores illo.
-                Dolores, maxime optio ipsam neque similique at ea, quod fugiat,
-                vitae vero illum voluptate laudantium doloribus. Sit molestias
-                eligendi voluptate, aperiam voluptas corrupti ex aspernatur
-                quisquam deserunt esse illo blanditiis atque sunt quos culpa!
-                Fuga assumenda pariatur nihil eum ea.
-              </p>
+              <p>{book.description}</p>
             </div>
           </div>
         </div>
